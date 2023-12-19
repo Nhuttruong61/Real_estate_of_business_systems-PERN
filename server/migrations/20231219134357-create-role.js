3df894133c4043f78aeb3b133a2e71d6a59e934c
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-12-19 21:06:34 +0700

    build model bacsic

---
+++ server/migrations/20231219134357-create-role.js
@@ -0,0 +1,32 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Roles", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4(),
+      },
+      code: {
+        type: Sequelize.INTEGER,
+      },
+      value: {
+        type: Sequelize.STRING,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Roles");
+  },
+};
