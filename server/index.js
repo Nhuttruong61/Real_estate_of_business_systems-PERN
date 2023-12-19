d6b58a753120ffa71e5177f4d8f1886c630e6c11
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-12-19 16:07:05 +0700

    setup project

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json({}));
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
